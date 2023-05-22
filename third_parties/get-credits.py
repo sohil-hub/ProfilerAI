c8014564d17f9e2f8a6fd035c9d2ac87044624fa
sohil@venturit <sohil.sh@venturit.com>
2023-05-22 17:01:24 +0530

    Revert "formatted code"
    This reverts commit daa31aeb038ae1b486839da72ff9653edf7f77e5.

--- third_parties/get-credits.py
+++ third_parties/get-credits.py
@@ -1,14 +1,16 @@
 import requests
 
-api_endpoint = "https://nubela.co/proxycurl/api/linkedin/profile/resolve"
-api_key = "tKsS1B1OtvqMTFymzvIFvQ"
-header_dic = {"Authorization": "Bearer " + api_key}
+api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/profile/resolve'
+api_key = 'tKsS1B1OtvqMTFymzvIFvQ'
+header_dic = {'Authorization': 'Bearer ' + api_key}
 params = {
-    "company_domain": "gatesfoundation.org",
-    "first_name": "Bill",
-    "enrich_profile": "enrich",
-    "location": "Seattle",
-    "title": "Co-chair",
-    "last_name": "Gates",
+    'company_domain': 'gatesfoundation.org',
+    'first_name': 'Bill',
+    'enrich_profile': 'enrich',
+    'location': 'Seattle',
+    'title': 'Co-chair',
+    'last_name': 'Gates',
 }
-response = requests.get(api_endpoint, params=params, headers=header_dic)
+response = requests.get(api_endpoint,
+                        params=params,
+                        headers=header_dic)
